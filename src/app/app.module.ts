33ab133e55ccca74d57ef57b2a4648ac7e32aa67
Mike Fawcett <mike.fawcett@gmail.com>
2019-05-13 11:41:22 -0400

    fixed initial styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WorkspaceMenuComponent } from './workspace-menu/workspace-menu.component';
+import { PresenceComponent } from './presence/presence.component';
+import { ChatMenuComponent } from './chat-menu/chat-menu.component';
+import { ChatWindowComponent } from './chat-window/chat-window.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkspaceMenuComponent,
+    PresenceComponent,
+    ChatMenuComponent,
+    ChatWindowComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/workspace-menu/workspace-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-workspace-menu',
+  templateUrl: './workspace-menu.component.html',
+  styleUrls: ['./workspace-menu.component.scss']
+})
+export class WorkspaceMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
