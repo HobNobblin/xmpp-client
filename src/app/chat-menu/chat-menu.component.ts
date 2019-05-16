2b81a5251ab56012674a476d52ab866882b099d5
Mike Fawcett <mike.fawcett@gmail.com>
2019-05-15 23:05:50 -0400

    added workspace menu

--- src/app/chat-menu/chat-menu.component.ts
+++ src/app/chat-menu/chat-menu.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-chat-menu',
-  templateUrl: './chat-menu.component.html',
-  styleUrls: ['./chat-menu.component.scss']
+    selector: 'app-chat-menu',
+    templateUrl: './chat-menu.component.html',
+    styleUrls: ['./chat-menu.component.scss']
 })
 export class ChatMenuComponent implements OnInit {
 
-  constructor() { }
+    channels = [
+        {
+            'name': 'development',
+            'newMessages': false,
+            'noticeCount': 0,
+        },
+        {
+            'name': 'devsecops',
+            'newMessages': true,
+            'noticeCount': 0,
+        },
+        {
+            'name': 'ems',
+            'newMessages': false,
+            'noticeCount': 0,
+        },
+        {
+            'name': 'ess',
+            'newMessages': true,
+            'noticeCount': 2,
+        },
+        {
+            'name': 'fedex-certification',
+            'newMessages': false,
+            'noticeCount': 0,
+        }
+    ];
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+    }
 
 }
--- src/app/workspace-menu/workspace-menu.component.ts
+++ src/app/workspace-menu/workspace-menu.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-workspace-menu',
-  templateUrl: './workspace-menu.component.html',
-  styleUrls: ['./workspace-menu.component.scss']
+    selector: 'app-workspace-menu',
+    templateUrl: './workspace-menu.component.html',
+    styleUrls: ['./workspace-menu.component.scss']
 })
+
 export class WorkspaceMenuComponent implements OnInit {
 
-  constructor() { }
+    workspaces = [
+        {
+            'id': '1',
+            'name': 'Neopost Software Development',
+            'initial': 'NS',
+            'newMessages': true,
+            'noticeCount': 2,
+        },
+        {
+            'id': '2',
+            'name': 'newhavenio',
+            'initial': 'N',
+            'newMessages': true,
+            'noticeCount': 0,
+        },
+        {
+            'id': '3',
+            'name': 'Neopost',
+            'initial': 'NE',
+            'newMessages': false,
+            'noticeCount': 0,
+        },
+        {
+            'id': '4',
+            'name': 'Thimble Island Brewing Company',
+            'initial': 'TI',
+            'newMessages': false,
+            'noticeCount': 0,
+        },
+    ];
 
-  ngOnInit() {
-  }
+    selectedSpace: String = '1';
+
+    constructor() { 
+    }
+
+    ngOnInit() {
+    }
+
+    workspaceSelected(workspaceID) {
+        this.selectedSpace = workspaceID;
+    }
 
 }
